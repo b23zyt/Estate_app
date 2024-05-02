12238af58c8c0f62e26bab0281d77885a20e12cd
Benjamin Zeng <benjaminzeng2110@gmail.com>
2024-05-03 03:00:25 +0800

    Update Post PostDetail model, update post controller

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+import NewPostPage from "./routes/newPostPage/newPostPage";
 
 
 function App() {
@@ -55,6 +56,10 @@ function App() {
         {
           path: "/profile/update",
           element: <ProfileUpdatePage />,
+        },
+        {
+          path: "/add",
+          element: <NewPostPage />,
         },
       ],
     },
