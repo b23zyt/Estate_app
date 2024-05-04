77401b26f153a6492d7286904c0fcecf5e8bb518
Benjamin Zeng <benjaminzeng2110@gmail.com>
2024-05-04 05:05:12 -0400

    Fetch data with react-router-dom and update Map component

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -4,8 +4,9 @@ import "leaflet/dist/leaflet.css";
 import Pin from '../pin/Pin';
 
 function Map({items}){
+  //check if it is on singlepage (show the exact latitude and longitude) or listpage(show toronto)
   return (
-    <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
+    <MapContainer center={items.length === 1 ? [items[0].latitude, items[0].longitude] : [43.651, -79.347]} zoom={7} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
