fff7ec7388d780bfa6774f8c41a7caa0b19a098f
Benjamin Zeng <benjaminzeng2110@gmail.com>
2024-05-04 20:48:52 -0400

    Achieve fetching profile post, there's an error with saveProfile

--- api/route/user.route.js
+++ api/route/user.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser, savePost } from "../controllers/user.controller.js";
+import { deleteUser, getUser, getUsers, updateUser, savePost, profilePosts } from "../controllers/user.controller.js";
 import {verifyToken} from "../middleware/verifyToken.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers); //get all users
-userRouter.get("/:id",verifyToken, getUser); //get single user
+// userRouter.get("/:id",verifyToken, getUser); //get single user
 userRouter.put("/:id", verifyToken, updateUser); //change profile
 userRouter.delete("/:id", verifyToken, deleteUser);
 userRouter.post("/save", verifyToken, savePost);
+userRouter.get("/profilePosts", verifyToken, profilePosts);
 
 export default userRouter;
